3ce72addb956cb62447b227f1fa73ce41f5c86f8
Elabar <leehoemun@gmail.com>
2019-03-16 20:08:27 +0800

    added more rubbish test case

--- src/test/java/TestBooking.java
+++ src/test/java/TestBooking.java
@@ -8,10 +8,18 @@ import static org.mockito.Mockito.*;
 @RunWith(JUnitParamsRunner.class)
 public class TestBooking {
 
+	//room_type=VIP/deluxe/standard
+	//member_type=VIP/normal/non
 	private Object[] dataForSetBooking(){
 		return new Object[] {
 			new Object[] {"VIP",true,3,0,"VIP",1},
-			new Object[] {"deluxe",true,2,2,"normal",0}
+			new Object[] {"deluxe",true,3,0,"VIP",1},
+			new Object[] {"standard",true,3,0,"VIP",1},
+			
+			new Object[] {"deluxe",true,2,2,"normal",0},
+			new Object[] {"deluxe",true,2,1,"normal",1},
+			new Object[] {"standard",false,1,0,"non",0},
+			new Object[] {"standard",true,1,0,"non",1}
 		};
 	}
 	
@@ -19,7 +27,6 @@ public class TestBooking {
 	@Parameters(method = "dataForSetBooking")
 	public void testSetBooking(String room_type,boolean isRoomAvailable,int maxBookedRoom,int bookedRoom,String memberType,int expected) {
 		User user = mock(User.class);
-		when(user.getMember_type()).thenReturn(room_type);
 		when(user.getMaxNumberOfBookedRoom()).thenReturn(maxBookedRoom);
 		when(user.getNumberOfBookedRoom()).thenReturn(bookedRoom);
 		when(user.getMember_type()).thenReturn(memberType);
